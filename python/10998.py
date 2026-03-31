dcaf49b9ca3a15f50a3e067f1b4d3bbe86bd2af1
chldngurdlqslek <boogie1233@naver.com>
2026-03-31 10:47:10 +0900

    python coding folder

--- python/10998.py
+++ python/10998.py
@@ -4,8 +4,52 @@
 #A= int(input('number= '))
 #B= int(input('number= '))
 
-A= int(input('number= '))
-B= int(input('number= '))
+#A= int(input(0<A))
+#B= int(input(B))
 #숫자 형변환
+#print(A+B)
 
-print(A+B)
+
+#if 조건식:
+#    조건이 참일때 실행됩니다
+#1
+#
+#3
+
+#A = input().split()
+#A=int(A)
+
+#if A % 4 & A % 400  == 0:
+#    print('1')
+#if A % 100 == 0:
+#    print('0')
+#else:
+#    print('0')
+
+#반복문
+#A = [1, 2, 3, 4, 5]
+
+#if 6 in A:
+#    print('2')
+
+#A= input()
+#A= int(A)
+
+#print(A, "*", 1, "=", A*1)
+#print(A, "*", 2, "=", A*2)
+#print(A, "*", 3, "=", A*3)
+#print(A, "*", 4, "=", A*4)
+#print(A, "*", 5, "=", A*5)
+#print(A, "*", 6, "=", A*6)
+#print(A, "*", 7, "=", A*7)
+#print(A, "*", 8, "=", A*8)
+#print(A, "*", 9, "=", A*9)
+
+#반복문
+
+A= input()
+A= int(A)
+
+for i in range(1, 10):
+    #print(i+1)
+    print(A, "*", i, "=", A*i)
---
+++ python/8393.py
@@ -0,0 +1,11 @@
+A= int(input())
+
+#while, for
+# 1 ~ A 출력
+
+sum = 0
+for i in range(1, A+1):
+    sum= sum + i
+    #print(i)
+    
+print(sum)
